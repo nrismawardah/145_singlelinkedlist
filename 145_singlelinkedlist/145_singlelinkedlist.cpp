580b8adac0a6b531a1991e3128d3caaf61d2c547
nrismawardah <nrismawardah@gmail.com>
2023-05-18 16:41:56 +0700

    Mengembalikan nilai false

--- 145_singlelinkedlist/145_singlelinkedlist.cpp
+++ 145_singlelinkedlist/145_singlelinkedlist.cpp
@@ -73,5 +73,8 @@ bool serachNode(int nim, Node* current, Node* previous) {
 	}
 }
 bool deleteNode(int nim) {
-
+	Node* current = START;
+	Node* previous = START;
+	if (serachNode(nim, previous, current) == false)
+		return false;
 }
